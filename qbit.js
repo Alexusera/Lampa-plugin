06872624fa7649c5b782f759e89868a6ad9ffa5d
Alexusera <alexaandr.primak@gmail.com>
2026-06-24 19:47:31 +0300

    Update qbit.js

--- qbit.js
+++ qbit.js
@@ -1,31 +1,32 @@
 (function () {
     'use strict';
 
-    // 1. Создаем функцию инициализации плагина
     function startPlugin() {
         var qbit_url = 'http://192.168.88.247:8080';
 
-        // 2. Обязательная регистрация в системе Лампы
+        // Регистрируем плагин в системе Лампы
         Lampa.Plugins.add({
             id: 'qbit_remote',
             name: 'qBittorrent Remote',
             description: 'Отправка торрентов на ПК',
-            version: '1.0.0',
+            version: '1.0.3',
             author: 'Mikhail'
         });
 
-        // 3. Перехват клика по торренту
+        // Отслеживаем клик по торренту
         Lampa.Listener.follow('torrent', function (e) {
             if (e.name === 'select' && e.element && e.element.magnet) {
-                e.ghost = true; // Отменяем стандартное открытие плеера
+                e.ghost = true; // Отменяем стандартный запуск плеера
                 Lampa.Noty.show('Отправка на ПК...');
 
+                // Генерируем boundary старым методом без ES6-строк
                 var boundary = '----WebKitFormBoundary' + Math.random().toString(36).substring(2);
                 var xhr = new XMLHttpRequest();
                 
                 xhr.open('POST', qbit_url + '/api/v2/torrents/add', true);
                 xhr.setRequestHeader('Content-Type', 'multipart/form-data; boundary=' + boundary);
 
+                // Собираем тело запроса обычной конкатенацией строк (+)
                 var body = '--' + boundary + '\r\n' +
                            'Content-Disposition: form-data; name="urls"\r\n\r\n' +
                            e.element.magnet + '\r\n' +
@@ -33,7 +34,7 @@
 
                 xhr.onload = function () {
                     if (xhr.status === 200 || xhr.responseText === 'Ok.') {
-                        Lampa.Noty.show('Успешно отправлено в qBit!');
+                        Lampa.Noty.show('Успешно отправлено!');
                     } else {
                         Lampa.Noty.show('Ошибка qBit: ' + xhr.status);
                     }
@@ -48,7 +49,7 @@
         });
     }
 
-    // 4. Правильный запуск внутри движка Лампы
+    // Запуск строго по готовности приложения
     if (window.appready) {
         startPlugin();
     } else {
