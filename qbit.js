90d2eb018745fa9831126bcd591a42b5851c38a2
Alexusera <alexaandr.primak@gmail.com>
2026-06-24 22:41:31 +0300

    Update qbit.js

--- qbit.js
+++ qbit.js
@@ -1,55 +1,71 @@
+/**
+ * @plugin qbit_remote
+ * @title qBittorrent Remote
+ * @description Отправка торрентов на ПК
+ * @version 1.0.9
+ * @author Mikhail
+ */
+
 (function () {
     'use strict';
 
-    // Регистрируем плагин сразу, используя чистый стандарт Лампы
-    try {
+    function startPlugin() {
+        var qbit_url = 'http://192.168.88.247:8080';
+
+        // 1. Регистрируем плагин в системе, чтобы Лампа видела его статус
         Lampa.Plugins.add({
             id: 'qbit_remote',
             name: 'qBittorrent Remote',
             description: 'Отправка торрентов на ПК',
-            version: '1.0.7',
-            author: 'Mikhail',
-            ready: function () {
-                // Слушаем клики по торрентам только после успешной регистрации
-                Lampa.Listener.follow('torrent', function (e) {
-                    if (e.name === 'select' && e.element && e.element.magnet) {
-                        e.ghost = true;
-                        Lampa.Noty.show('Отправка на ПК...');
+            version: '1.0.9',
+            author: 'Mikhail'
+        });
 
-                        // Прячем IP-адрес глубоко внутри события, чтобы WebView не ругался при старте
-                        var host = '192.168.88.247';
-                        var port = '8080';
-                        var qbit_url = 'http://' + host + ':' + port;
+        // 2. Перехватываем клик по торренту
+        Lampa.Listener.follow('torrent', function (e) {
+            // Если пользователь выбрал торрент (select) и у него есть магнет-ссылка
+            if (e.name === 'select' && e.element && e.element.magnet) {
+                
+                // e.ghost = true запрещает Лампе запускать стандартный плеер
+                e.ghost = true; 
+                
+                Lampa.Noty.show('Отправка на ПК...');
 
-                        var boundary = '----WebKitFormBoundary' + Math.random().toString(36).substring(2);
-                        var xhr = new XMLHttpRequest();
-                        
-                        xhr.open('POST', qbit_url + '/api/v2/torrents/add', true);
-                        xhr.setRequestHeader('Content-Type', 'multipart/form-data; boundary=' + boundary);
+                // Формируем FormData запрос к API qBittorrent
+                var boundary = '----WebKitFormBoundary' + Math.random().toString(36).substring(2);
+                var xhr = new XMLHttpRequest();
+                
+                xhr.open('POST', qbit_url + '/api/v2/torrents/add', true);
+                xhr.setRequestHeader('Content-Type', 'multipart/form-data; boundary=' + boundary);
 
-                        var body = '--' + boundary + '\r\n' +
-                                   'Content-Disposition: form-data; name="urls"\r\n\r\n' +
-                                   e.element.magnet + '\r\n' +
-                                   '--' + boundary + '--\r\n';
+                var body = '--' + boundary + '\r\n' +
+                           'Content-Disposition: form-data; name="urls"\r\n\r\n' +
+                           e.element.magnet + '\r\n' +
+                           '--' + boundary + '--\r\n';
 
-                        xhr.onload = function () {
-                            if (xhr.status === 200 || xhr.responseText === 'Ok.') {
-                                Lampa.Noty.show('Успешно отправлено!');
-                            } else {
-                                Lampa.Noty.show('Ошибка qBit: ' + xhr.status);
-                            }
-                        };
-
-                        xhr.onerror = function () {
-                            Lampa.Noty.show('Ошибка сети. Проверьте ПК.');
-                        };
-
-                        xhr.send(body);
+                xhr.onload = function () {
+                    if (xhr.status === 200 || xhr.responseText === 'Ok.') {
+                        Lampa.Noty.show('Успешно отправлено!');
+                    } else {
+                        Lampa.Noty.show('Ошибка qBit: ' + xhr.status);
                     }
-                });
+                };
+
+                xhr.onerror = function () {
+                    Lampa.Noty.show('Ошибка сети. Проверьте WebUI на ПК.');
+                };
+
+                xhr.send(body);
             }
         });
-    } catch (e) {
-        console.error('qBit Plugin Error: ', e);
+    }
+
+    // Ждем полной готовности приложения Лампа перед запуском логики
+    if (window.appready) {
+        startPlugin();
+    } else {
+        Lampa.Listener.follow('app', function (e) {
+            if (e.name === 'ready') startPlugin();
+        });
     }
 })();
