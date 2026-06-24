b1d3584ad9bc31a4233b850f32287a01caf933aa
Alexusera <alexaandr.primak@gmail.com>
2026-06-24 19:39:42 +0300

    Update qbit.js

--- qbit.js
+++ qbit.js
@@ -1,20 +1,24 @@
 (function () {
     'use strict';
 
-    function startPlugin() {
+    // Создаем плагин внутри глобального объекта Lampa
+    window.plugin_qbit_remote_ready = function () {
         var qbit_url = 'http://192.168.88.247:8080';
 
+        // Обязательная регистрация в списке расширений Лампы
         Lampa.Plugins.add({
-            id: 'qbit_custom',
+            id: 'qbit_remote',
             name: 'qBittorrent Remote',
             description: 'Отправка торрентов на ПК',
-            version: '1.0.1',
+            version: '1.0.0',
             author: 'Mikhail'
         });
 
+        // Слушаем событие клика по торренту
         Lampa.Listener.follow('torrent', function (e) {
             if (e.name === 'select' && e.element && e.element.magnet) {
-                e.ghost = true;
+                // Перехватываем управление (не даем запускаться встроенному плееру)
+                e.ghost = true; 
                 Lampa.Noty.show('Отправка на ПК...');
 
                 var boundary = '----WebKitFormBoundary' + Math.random().toString(36).substring(2);
@@ -24,13 +28,13 @@
                 xhr.setRequestHeader('Content-Type', 'multipart/form-data; boundary=' + boundary);
 
                 var body = '--' + boundary + '\r\n' +
-                           'Content-Disposition: form-data; name=\"urls\"\r\n\r\n' +
+                           'Content-Disposition: form-data; name="urls"\r\n\r\n' +
                            e.element.magnet + '\r\n' +
                            '--' + boundary + '--\r\n';
 
                 xhr.onload = function () {
                     if (xhr.status === 200 || xhr.responseText === 'Ok.') {
-                        Lampa.Noty.show('Успешно отправлено!');
+                        Lampa.Noty.show('Успешно отправлено в qBit!');
                     } else {
                         Lampa.Noty.show('Ошибка qBit: ' + xhr.status);
                     }
@@ -43,8 +47,13 @@
                 xhr.send(body);
             }
         });
-    }
+    };
 
-    if (window.appready) startPlugin();
-    else Lampa.Listener.follow('app', function (e) { if (e.name === 'ready') startPlugin(); });
+    // Механизм запуска, который требует движок Лампы
+    if (window.Lampa) {
+        window.plugin_qbit_remote_ready();
+    } else {
+        window.plugin_qbit_remote_ready_init = window.plugin_qbit_remote_ready;
+    }
 })();
+    
