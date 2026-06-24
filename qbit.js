574a27ff2bbce566030a2915fd82e9264ef48e3e
Alexusera <alexaandr.primak@gmail.com>
2026-06-24 19:50:10 +0300

    Update qbit.js

--- qbit.js
+++ qbit.js
@@ -1,32 +1,30 @@
 (function () {
     'use strict';
 
-    function startPlugin() {
+    function startQbitRemote() {
         var qbit_url = 'http://192.168.88.247:8080';
 
-        // Регистрируем плагин в системе Лампы
+        // Стандартная регистрация плагина, которую парсер Лампы гарантированно считает
         Lampa.Plugins.add({
             id: 'qbit_remote',
             name: 'qBittorrent Remote',
             description: 'Отправка торрентов на ПК',
-            version: '1.0.3',
+            version: '1.0.4',
             author: 'Mikhail'
         });
 
-        // Отслеживаем клик по торренту
+        // Слушатель событий торрента
         Lampa.Listener.follow('torrent', function (e) {
             if (e.name === 'select' && e.element && e.element.magnet) {
-                e.ghost = true; // Отменяем стандартный запуск плеера
+                e.ghost = true; // Блокируем стандартный плеер Лампы
                 Lampa.Noty.show('Отправка на ПК...');
 
-                // Генерируем boundary старым методом без ES6-строк
                 var boundary = '----WebKitFormBoundary' + Math.random().toString(36).substring(2);
                 var xhr = new XMLHttpRequest();
                 
                 xhr.open('POST', qbit_url + '/api/v2/torrents/add', true);
                 xhr.setRequestHeader('Content-Type', 'multipart/form-data; boundary=' + boundary);
 
-                // Собираем тело запроса обычной конкатенацией строк (+)
                 var body = '--' + boundary + '\r\n' +
                            'Content-Disposition: form-data; name="urls"\r\n\r\n' +
                            e.element.magnet + '\r\n' +
@@ -49,12 +47,10 @@
         });
     }
 
-    // Запуск строго по готовности приложения
-    if (window.appready) {
-        startPlugin();
+    // Официальный метод инициализации расширений в Лампе
+    if (window.Lampa) {
+        startQbitRemote();
     } else {
-        Lampa.Listener.follow('app', function (e) {
-            if (e.name === 'ready') startPlugin();
-        });
+        window.plugin_qbit_remote_ready = startQbitRemote;
     }
 })();
