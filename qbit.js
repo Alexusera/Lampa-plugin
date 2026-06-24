d0094d95df3605ae1e122a21d6e833c65784cc6d
Alexusera <alexaandr.primak@gmail.com>
2026-06-24 21:52:35 +0300

    Update qbit.js

--- qbit.js
+++ qbit.js
@@ -9,55 +9,15 @@
 (function () {
     'use strict';
 
-    function startPlugin() {
-        var qbit_url = 'http://192.168.88.247:8080';
-
-        Lampa.Plugins.add({
-            id: 'qbit_remote',
-            name: 'qBittorrent Remote',
-            description: 'Отправка торрентов на ПК',
-            version: '1.0.5',
-            author: 'Mikhail'
-        });
-
-        Lampa.Listener.follow('torrent', function (e) {
-            if (e.name === 'select' && e.element && e.element.magnet) {
-                e.ghost = true;
-                Lampa.Noty.show('Отправка на ПК...');
-
-                var boundary = '----WebKitFormBoundary' + Math.random().toString(36).substring(2);
-                var xhr = new XMLHttpRequest();
-                
-                xhr.open('POST', qbit_url + '/api/v2/torrents/add', true);
-                xhr.setRequestHeader('Content-Type', 'multipart/form-data; boundary=' + boundary);
-
-                var body = '--' + boundary + '\r\n' +
-                           'Content-Disposition: form-data; name="urls"\r\n\r\n' +
-                           e.element.magnet + '\r\n' +
-                           '--' + boundary + '--\r\n';
-
-                xhr.onload = function () {
-                    if (xhr.status === 200 || xhr.responseText === 'Ok.') {
-                        Lampa.Noty.show('Успешно отправлено!');
-                    } else {
-                        Lampa.Noty.show('Ошибка qBit: ' + xhr.status);
-                    }
-                };
-
-                xhr.onerror = function () {
-                    Lampa.Noty.show('Ошибка сети. Проверьте ПК.');
-                };
-
-                xhr.send(body);
-            }
-        });
-    }
-
-    if (window.appready) {
-        startPlugin();
-    } else {
-        Lampa.Listener.follow('app', function (e) {
-            if (e.name === 'ready') startPlugin();
-        });
-    }
+    // Простейшая инициализация плагина для проверки
+    Lampa.Plugins.add({
+        id: 'qbit_remote',
+        name: 'qBittorrent Remote',
+        description: 'Отправка торрентов на ПК',
+        version: '1.0.5',
+        author: 'Mikhail',
+        ready: function() {
+            console.log('qBit Remote Ready');
+        }
+    });
 })();
